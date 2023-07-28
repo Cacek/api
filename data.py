297cbcd58b8cf10699c78ad5233464c2107f0b99
jacek_kuchta <jacek@test.com>
2023-07-28 12:44:17 +0200

    add the required improvements

--- data.py
+++ data.py
@@ -1,27 +1,48 @@
 import requests
+from dataclasses import dataclass
 
 
-def get_data_from_api():
-    url = "https://randomuser.me/api/?results=10" #Przykladowe API
-    response = requests.get(url)
-    api_data = response.json()
-    return api_data["results"]
+@dataclass
+class UserData:
+    first_name: str
+    last_name: str
+    email: str
+    city: str
+    country: str
+
+    def __str__(self):
+        return f"Name: {self.first_name} {self.last_name}\nEmail: {self.email}\nLocation: {self.city}, {self.country}\n"
 
 
-def display_data(api_data):
-    for user in api_data:
-        print("First Name:", user["name"]["first"])
-        print("Last Name:", user["name"]["last"])
-        print("Email:", user["email"])
-        print("City:", user["location"]["city"])
-        print("Country:", user["location"]["country"])
-        print(20 * "-")
+class APIClient:
+    def __init__(self):
+        self.base_url = "https://randomuser.me/api/"
 
+    def get_data_from_api(self, results=10):
+        url = f"{self.base_url}?results={results}"
+        response = requests.get(url)
+        if response.ok:
+            return response.json()["results"]
+        else:
+            print("Error: Failed to fetch data from the API.")
+            return []
 
-def main():
-    api_data = get_data_from_api()
-    display_data(api_data)
+    def create_user_instance(self, user_data):
+        return UserData(
+            first_name=user_data["name"]["first"],
+            last_name=user_data["name"]["last"],
+            email=user_data["email"],
+            city=user_data["location"]["city"],
+            country=user_data["location"]["country"],
+        )
+
+    def run(self, results=10):
+        api_data = self.get_data_from_api(results)
+        user_instances = [self.create_user_instance(user_data) for user_data in api_data]
+        for user in user_instances:
+            print(user)
 
 
 if __name__ == "__main__":
-    main()
+    api_client = APIClient()
+    api_client.run()
